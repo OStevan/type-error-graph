0385c2430f875c27ec21d37b2f0cbbf323a7926d
Stevan Ognjanovic <stevan.ognjanovic@epfl.ch>
2019-04-04 16:58:22 +0200

    Added constraint language structure

---
+++ src/main/scala/ch/epfl/ognjanovic/stevan/error_messages/constraint_language/ConstraintLanguage.scala
@@ -0,0 +1,29 @@
+package ch.epfl.ognjanovic.stevan.error_messages.constraint_language
+import inox.parser.elaboration.{Constraints, SimpleTypes}
+
+trait ConstraintLanguage { self: Constraints with SimpleTypes =>
+
+  import SimpleTypes._
+  import ConstraintConjuctions._
+  import Hypotheses._
+
+  object ConstraintConjuctions {
+    case class ConstraintConjuction(constaints: Seq[Constraint])
+  }
+
+  sealed trait Axiom
+  object Axioms {
+    case class Simple(constraint: Constraint) extends Axiom
+    case class QuantifiedAxiom(quantifiedVariables: Seq[Unknown], constraintConjuction: ConstraintConjuction,
+                               constraint: Constraint) extends Axiom
+  }
+
+  object Hypotheses {
+    case class Hypothesis(axioms: Seq[Axiom])
+  }
+
+  object Assertions {
+    case class Assertion(hypothesis: Hypothesis, constraint: Constraint)
+  }
+
+}
